2df8b2d0ee2f758ae4579a75fbf1e22c320fcdd6
aayman01 <ayman.alfi098@gmail.com>
2024-08-17 18:25:33 +0600

    private route created

---
+++ src/Router/PrivateRoute.jsx
@@ -0,0 +1,24 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAuth from "../Hooks/useAuth";
+import { ClipLoader } from "react-spinners";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <ClipLoader color="#076cec" size={50} />
+      </div>
+    );
+  }
+
+  if (user) {
+    return children;
+  }
+
+  return <Navigate state={location.pathname} to="/login" replace></Navigate>;
+};
+
+export default PrivateRoute;
--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -3,24 +3,29 @@ import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import Login from "../Pages/Auth/login";
 import Register from "../Pages/Auth/Register";
+import PrivateRoute from "./PrivateRoute";
 
 export const router = createBrowserRouter([
-    {
-        path: '/',
-        element: <Main/>,
-        children: [
-            {
-                path: "/",
-                element: <Home/>
-            },
-            {
-                path: '/login',
-                element: <Login/>
-            },
-            {
-                path: '/register',
-                element: <Register/>
-            }
-        ]
-    }
-])
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        path: "/",
+        element: (
+          <PrivateRoute>
+            <Home />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+    ],
+  },
+]);
