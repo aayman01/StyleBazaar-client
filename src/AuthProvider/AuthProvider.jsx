2611bcfff48ad1bfba5c8d192671710a9087710b
aayman01 <ayman.alfi098@gmail.com>
2024-08-17 18:30:02 +0600

    loading issue fixed

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -46,6 +46,7 @@ const AuthProvider = ({children}) => {
   useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false)
     });
     return () => {
       unSubscribe();
